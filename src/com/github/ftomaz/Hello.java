e6f6c95d8c3c6b19e2acfdd45e1bee13e2a1eb4d
Fabio <Fabio@FTOMAZ>
2011-08-30 05:18:20 -0300

    testes - commit com git 2

--- src/com/github/ftomaz/Hello.java
+++ src/com/github/ftomaz/Hello.java
@@ -3,7 +3,7 @@ package com.github.ftomaz;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello - commit com git");
 	}
 
 }
